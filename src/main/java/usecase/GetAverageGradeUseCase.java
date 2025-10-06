a790dc6ae0d349945a5194acd0ff73c0c869e9d2
Ryan-Xue2 <ryan.xue2@gmail.com>
2025-10-06 10:07:00 -0400

    calculate average course grade

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -24,10 +24,18 @@ public final class GetAverageGradeUseCase {
         float sum = 0;
         int count = 0;
         // TODO Task 3b: Go to the MongoGradeDataBase class and implement getMyTeam.
+
         final Team team = gradeDataBase.getMyTeam();
         // Call the API to get all the grades for the course for all your team members
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
+
+        GetGradeUseCase useCase = new GetGradeUseCase(gradeDataBase);
+
+        for (String member: team.getMembers()) {
+            count++;
+            sum += useCase.getGrade(member, "207").getGrade();
+        }
 
         if (count == 0) {
             return 0;
